3915958a5eb479f25cd0b9b19d13d9f1aae4a610
sdxsd <willmaguire@icloud.com>
2023-02-24 14:59:18 +0100

    Guerilla Gardening.

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -1,1 +1,36 @@
 #include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(void): AForm("Presidential Pardon Form", 145, 137) {
+	target = "barbary";
+	isSigned = false;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string targetName):
+	AForm("Shrubbery Creation Form", 145, 137) {
+	target = targetName;
+	isSigned = false;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& toCopy):
+	AForm(toCopy.formName, toCopy.reqSign, toCopy.reqExec) {
+	this->isSigned = toCopy.isSigned;
+	this->target = toCopy.target;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(void) {
+	;
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& toCopy) {
+	this->isSigned = toCopy.isSigned;
+	return (*this);
+}
+
+void ShrubberyCreationForm::execute(Bureaucrat const & executor) const {
+	if (czechCredentialsExec(executor) == 1 && isSigned == true) {
+		std::cout << target << " has been pardoned by Zaphod Beeblebrox" << std::endl;
+	}
+	else {
+		throw ExecutionWhileUnsignedException();
+	}
+}
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -1,6 +1,17 @@
 #ifndef SHRUBBERYCREATIONFORM_H
 #define SHRUBBERYCREATIONFORM_H
 
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
 
+class ShrubberyCreationForm : public AForm {
+		public:
+		ShrubberyCreationForm(void);
+		ShrubberyCreationForm(std::string targetName);
+		ShrubberyCreationForm(const ShrubberyCreationForm& toCopy);
+		~ShrubberyCreationForm(void);
+		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& toCopy);
+		void execute(Bureaucrat const & executor) const;
+};
 
 #endif // SHRUBBERYCREATIONFORM_H
